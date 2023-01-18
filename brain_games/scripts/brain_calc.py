e00ee6b616c7c1686b678dd66359b0d7e107b211
Evgeniy Gordienko <evg.gordienko@gmail.com>
2023-01-18 08:17:06 +0300

    Step 6 start

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import prompt
+import random
+
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+def get_answer():
+    rand_number1 = random.randint(1, 100)
+    rand_number2 = random.randint(1, 100)
+    action = random.choice(['plus', 'minus', 'multiply'])
+    answer = prompt.string(f'Question: {rand_number}\n')
+    return rand_number, answer
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    counter = 0
+    while counter < 3:
+        print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+        rand_number, answer = get_answer()
+        if (rand_number % 2 == 0 and answer == "yes") or (rand_number % 2 != 0 and answer == "no"):
+            print('Correct!')
+            counter += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
